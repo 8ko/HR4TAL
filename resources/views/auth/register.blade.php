75ebf44a7dad4aee56bc08e4f3bf8aa78d287bba
8ko <8ko.exe@gmail.com>
2022-04-12 20:43:41 +0800

    admin create

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -10,6 +11,25 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+
+                        <div class="row mb-3">
+                            <label for="employee_id" class="col-md-4 col-form-label text-md-end">{{ __('User Level') }}</label>
+
+                            <div class="col-md-6">
+                            <select class="custom-select-lg form-select @error('employee_id') is-invalid @enderror" id="user_level" name="user_level" value="{{ old('user_level') }}" required autocomplete="user_levelssssss" autofocus>
+                                <option value="admin">Administrator</option>
+                                <option value="engr">Engineer</option>
+                                <option value="hr">Human Resource</option>
+                                <option selected value="user">Employee</option>
+                            </select>
+
+                                @error('employee_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="employee_id" class="col-md-4 col-form-label text-md-end">{{ __('Employee ID') }}</label>
@@ -56,9 +76,11 @@
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 input-group mb-3 input-group-append">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <span class="input-group-text" id="basic-addon2">@roc.ph</span>
 
+  
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -71,8 +93,12 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
+                                
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <i class="far fa-copy" onlick="copy()"></i>
+                                <span class="far fa-copy" onclick="copy()"></span>
+                                <span class="fas fa-copy" onclick="copy()"></span>
+                                <button class="btn btn-primary">Generate Password</button>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -81,18 +107,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Create Account') }}
                                 </button>
                             </div>
                         </div>
@@ -102,4 +120,6 @@
         </div>
     </div>
 </div>
+
+
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome-all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <link rel="stylesheet" href="{{ asset('assets/fonts/line-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/simple-line-icons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome5-overrides.min.css') }}">
